be159223f7180f4589e9d0db26e99198337f01c7
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-10-24 11:49:52 -0600

    Week 2 workshop, up to task 3

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -152,3 +152,28 @@ export const addFavorite = campsiteId => ({
   type: ActionTypes.ADD_FAVORITE,
   payload: campsiteId
 });
+
+
+
+
+
+export const addComment = comment => ({
+  type: ActionTypes.ADD_COMMENT,
+  payload: comment
+});
+
+
+export const postComment = (campsiteId, ratimg, author, text) => dispatch => {
+
+  const newComment = {
+    campsiteId,
+    rating,
+    author,
+    text
+}; 
+
+  setTimeout(() => {
+      dispatch(addComment(newComment));
+  }, 2000);
+};
+
