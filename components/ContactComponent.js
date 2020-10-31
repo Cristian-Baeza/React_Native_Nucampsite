a8c59f6e2b9e658d8d60a42f4bc6a322f42a72c2
Cristian-Baeza <cristianbaeza092@gmail.com>
2020-10-30 19:46:51 -0600

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+
 
 
 class Contact extends Component {
@@ -11,26 +13,28 @@ class Contact extends Component {
 
   static navigationOptions = {
     title: 'Contact us'
-}
+  }
 
 
-// Week 1 task 2 
+  // Week 1 task 2 
 
   render() {
     return (
       <ScrollView>
-        <Card
-          title={"Contact Information"}
-          wrapperStyle={{margin: 20}}
-        >
-          <Text>1 Nucamp Way</Text>
-          <Text>Seattle, WA 90838</Text>
-          <Text
-            style={{marginBottom : 10}}
-          >U.S.A</Text>
-          <Text>Phone: 206-3454-3455</Text>
-          <Text>Email: campsites@nucamp.co</Text>
-        </Card>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+          <Card
+            title={"Contact Information"}
+            wrapperStyle={{ margin: 20 }}
+          >
+            <Text>1 Nucamp Way</Text>
+            <Text>Seattle, WA 90838</Text>
+            <Text
+              style={{ marginBottom: 10 }}
+            >U.S.A</Text>
+            <Text>Phone: 206-3454-3455</Text>
+            <Text>Email: campsites@nucamp.co</Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
